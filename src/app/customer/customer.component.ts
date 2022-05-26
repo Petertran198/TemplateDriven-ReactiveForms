0548bdad1293f71fdfe71196a4329a7d5b20af5c
Peter Tran <petertran198@gmail.com>
2022-05-25 17:34:30 -0700

    Created customer interface

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+import { Customer } from "./customer.model";
 
 @Component({
   selector: "app-customers",
@@ -6,7 +9,14 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./customer.component.css"],
 })
 export class CustomersComponent implements OnInit {
+  customer = new Customer();
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit(): void {}
+
+  save(customerForm: NgForm): void {
+    console.log(customerForm.form);
+    console.log("Saved: " + JSON.stringify(customerForm.value));
+  }
 }
