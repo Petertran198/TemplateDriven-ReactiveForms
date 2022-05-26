41d09351433fba0c7db2c9faa4c2d15131bf79c0
Peter Tran <petertran198@gmail.com>
2022-05-25 17:32:32 -0700

    Inital form set up html template approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CustomersComponent } from './customers/customers.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CustomersComponent } from "./customer/customer.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CustomersComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CustomersComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
